2f9b7c958e2070545a43f14d2fbda8426b2629b4
crocsg <pub.sg@free.fr>
2025-11-11 17:35:47 +0100

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,8 @@ master_doc = 'index'
 project = u'AccessBrailleRAP'
 copyright = 'GPL V3 - BrailleRAP Team'
 
-version = '0.6.3'
-release = '0.6.3'
+version = '0.7.0'
+release = '0.7.0'
 exclude_patterns = ['_build']
 default_role = 'obj'
 pygments_style = 'sphinx'
